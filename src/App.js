3f520af2a64f35d4f634f9c0f82e97a5bcd71765
mark <heidmetsmark@gmail.com>
2022-11-05 16:05:03 +0200

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-const App = () => (
-        <div className="App">
-            <header className="App-header">
-            </header>
+import React from "react"
+import { TopBar } from "./components/TopBar"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+// import Home from "./pages/Home"
+// import Projects from "./pages/Projects"
+// import Contact from "./pages/Contact"
+// import NotFound from "./pages/NotFound"
+// import Footer from "./components/Footer"
+// import ScrollWrapper from "./components/ScrollWrapper"
+
+const App = () => {
+    return (
+        <div id="App">
+            <Router>
+                {/* <ScrollWrapper> */}
+                    <TopBar />
+                    <Routes>
+                        <Route path="/" element={<Dashboard />} />
+                        <Route path="/logistics" element={<Logistics />} />
+                        <Route path="/leaderboard" element={<Leaderboard />} />
+                        <Route path="/devices" element={<Devices />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                    {/* <Footer /> */}
+                {/* </ScrollWrapper> */}
+            </Router>
         </div>
-)
+    )
+}
 
 export default App;
