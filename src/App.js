8eaec95b4b66a3dd53a36549a26f508742ae6f63
mark <heidmetsmark@gmail.com>
2022-11-05 18:25:18 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { TopBar } from "./components/TopBar"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import {Leaderboard} from "./components/Leaderboard"
 // import Home from "./pages/Home"
 // import Projects from "./pages/Projects"
 // import Contact from "./pages/Contact"
@@ -18,7 +19,7 @@ const App = () => {
                     <Routes>
                         {/* <Route path="/" element={<Dashboard />} /> */}
                         {/* <Route path="/logistics" element={<Logistics />} /> */}
-                        {/* <Route path="/leaderboard" element={<Leaderboard />} /> */}
+                        <Route path="/leaderboard" element={<Leaderboard />} />
                         {/* <Route path="/devices" element={<Devices />} /> */}
                         {/* <Route path="*" element={<NotFound />} /> */}
                     </Routes>
--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,6 +1,13 @@
+import { useState } from "react"
+
 export const Leaderboard = () => {
+    const [position, setPosition] = useState(5)
+
     return (
-        <>
-        </>
+        <div id="leaderboard">
+            <div>
+                <span>#</span><span style={{ fontSize: 38, fontWeight: "bold" }} id="pos">{ position }</span>
+            </div>
+        </div>
     )
 }
