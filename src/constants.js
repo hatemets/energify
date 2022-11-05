6e693b39bf533dfcd1142690ec3957860d767cd8
mark <heidmetsmark@gmail.com>
2022-11-06 01:08:29 +0200

    Updates

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,4 @@ export const GreenDarker = "#004d25"
 export const GreenRegular = "#1bb565"
 export const BoxShadow = "rgba(0, 0, 0, 0.24) 0px 3px 12px"
 export const LightGrey = "#dedede"
+export const navbarHeight = 89.31
